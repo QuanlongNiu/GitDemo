8a2ddfdffb8341a08bc26b9e969d1a4149e15721
nql <quanlongniu@163.com>
2024-06-20 16:44:51 +0800

    first commit

---
+++ src/main/java/com/nql/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.nql.git;
+
+/**
+ * @author 牛权龙
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
